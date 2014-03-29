801bb71dc7633f04568aa4769ee3c0e342cee5fe
Alex Taylor <alexmckenley@gmail.com>
2014-03-28 20:41:29 -0700

    implemented bubbleSort

---
+++ bubbleSort.js
@@ -0,0 +1,18 @@
+// BubbleSort
+
+var bubbleSort = function (numbers) {
+  for (var i = numbers.length - 1; i >= 0; i--) {
+    for (var j = 0; j < i; j++) {
+      if (numbers[j] > numbers[j + 1]) {
+        //swap
+        var temp = numbers[j];
+        numbers[j] = numbers[j + 1];
+        numbers[j + 1] = temp;
+      }
+    }
+  }
+
+  return numbers;
+};
+
+console.log(bubbleSort([4, 5, 6, 2, 1, 3, 5, 6]));
