c9c40c47baf237686b01708d8df85e3b0fb22eda
Alex Taylor <alexmckenley@gmail.com>
2014-03-29 14:50:54 -0700

    begin shellSort.js

--- shellSort.js
+++ shellSort.js
@@ -1,1 +1,13 @@
 // Shell Sort
+
+var shellSort = function (numbers) {
+  var n = numbers.length;
+  var gaps = [5, 3, 1];
+
+  gaps.forEach(function (gap) {
+    for (var i = gap; i < n; i++)
+
+
+  });
+
+};
