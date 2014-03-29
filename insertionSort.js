1c96b3505bbf8cff2eb52edcd9584df88e2b1fc1
Alex Taylor <alexmckenley@gmail.com>
2014-03-28 20:25:45 -0700

    implemented insertionSort

--- insertionSort.js
+++ insertionSort.js
@@ -2,7 +2,19 @@
 
 var insertionSort = function (numbers) {
   for (var i = 1; i < numbers.length; i++) {
-
+    for (var j = i; j > 0; j--) {
+      if (numbers[j] >= numbers[j - 1]) {
+        break;
+      } else {
+        //swap
+        var temp = numbers[j];
+        numbers[j] = numbers[j - 1];
+        numbers[j - 1] = temp;
+      }
+    }
   }
+  return numbers;
 
 };
+
+console.log(insertionSort([6, 4, 2, 3, 7]));
